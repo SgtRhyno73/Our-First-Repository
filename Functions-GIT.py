ab4fa32a5f93eb23723249c0ad2009d99545271f
Keegan Killian <02keegan.killian@gulllakecs.org>
2019-02-11 09:54:29 -0500

    Add a Greetings Function

---
+++ Functions-GIT.py
@@ -0,0 +1,12 @@
+#4.13.3: Greeting
+#Keegan Killian
+#2.6.19
+
+name = input("What is your name?: ")
+
+def greeting():
+    print("Hi there " + name + "!")
+    print("Nice to meet you!")
+
+greeting()
+
