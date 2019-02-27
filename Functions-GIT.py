bb0fa55c2562a5ab950e1fdadb627685c8b70f3a
Keegan Killian <02keegan.killian@gulllakecs.org>
2019-02-27 09:59:37 -0500

    add functions and variables, part 3

--- Functions-GIT.py
+++ Functions-GIT.py
@@ -1,8 +1,8 @@
-#4.13.3: Greeting
-#Keegan Killian
-#2.6.19
+# 4.13.3: Greeting
+# Keegan Killian
+# 2.6.19
 
-'''
+
 
 name = input("What is your name?: ")
 
@@ -12,11 +12,13 @@ def greeting():
 
 greeting()
 
-'''
+
 
 # 4.13.4: Functions and Variables
 # Keegan Killian
 # 2.14.19
+
+
 
 x = 2202
 
@@ -26,4 +28,32 @@ def print_something():
 
 print_something()
 print(x)
+
+
+
+# 4.13.5: Functions and Variables - part 2
+# Keegan Killian
+# 2.14.19
+
+my_variable = 3.14
+
+def something():
+    print(my_variable + 10)
+
+something()
+
+
+
+# 4.13.6: Functions and Variables, part 3
+# Keegan Killian
+# 2.18.19
+
+
+
+def print_number(x):
+    print(str(x))
+
+print_number(15)
+print_number('\n' + 'Hello World')
+
 
